c33c47a8b45ec74a5373287ef9773f192a1aa777
RuzlaJabir <ruzlajabir@gmail.com>
2024-11-15 10:36:40 -0800

    MongoDB setup modified

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,5 @@ connectDB();
 app.use(express.json());
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
