99439269a656f5e88fc89987c2674ee62a29dc2a
hanzulmj <hanzulyeenas99@gmail.com>
2024-11-22 12:18:50 +0530

    update adminlogin.page

--- frontend/src/components/Adminlogin.js
+++ frontend/src/components/Adminlogin.js
@@ -1,22 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Adminlogin = () =>{
+    const[adminId,setAdminId] = useState("");
+    const[password,setPassword] = useState("");
+    const[error,setError] = useState("");
+    const[showPassword,setShowPassword] = useState(false);
+    const[loading,setLoading]=useState(false);
+
+    const handleSubmit = async (e)=>{
+        //deafult page refresh
+        e.preventDefault();
+        setError("");
+
+        //input validation
+        if(!adminId || !password)
+        {
+            setError("Both fields are required");
+            return;
+        }
+        if(adminId === "adminuov123" && password ==="adminp123")
+        {
+            alert("Login Successful");
+        }
+        else
+        {
+            setError("Invalid AdminId or Password")
+        }
+    };
+
     return(
         <div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Admin Login</h1>
 
                 <div>
-                    <p>Admin ID</p>
-                    <input type="text" placeholder='' required/>
+                    <label>Admin ID</label>
+                    <input 
+                    type="text" 
+                    placeholder='Enter Admin ID'
+                    onChange={(e) => setAdminId(e.target.value)} 
+                    />
                 </div>
 
                 <div>
                     <p>Password</p>
-                    <input type="password" placeholder='' required/>
+                    <input 
+                    type="password" 
+                    placeholder="Enter Password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? "Hide" : "Show"}
+                    </span>
                 </div>
 
+                <div>
                 <button type="submit">Login</button>
+                </div>
+                
 
 
             </form>
