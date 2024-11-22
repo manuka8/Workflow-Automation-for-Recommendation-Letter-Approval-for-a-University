efd58409ac2846f8920b0382a6f563ee3f4ff693
sunshine <fathimashakeeka345@gmail.com>
2024-11-22 23:30:39 +0530

    starting with staffLogin

--- frontend/src/pages/Stafflogin.js
+++ frontend/src/pages/Stafflogin.js
@@ -1,1 +1,21 @@
+import React, { useState} from "react";
+ 
+ const Stafflogin = () => {
+    const [staffId, setStaffId] = useState(""); 
+    const [password, setPassword] = useState("");
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log("Staff Login Details:", { staffId, password });
+      
+    };
+
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default Stafflogin
  
