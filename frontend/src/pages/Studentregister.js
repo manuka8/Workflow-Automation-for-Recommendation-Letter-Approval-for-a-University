9b962fbcdfb4c76269e167d1eaefb8164cddd81c
hanzulmj <hanzulyeenas99@gmail.com>
2024-12-09 12:20:44 +0530

    I have done the basic structure for the file and varaible declaration for the StudentRegister.js file

---
+++ frontend/src/pages/Studentregister.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+const StudentRegister = () => {
+    const [studentId, setStudentId] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [faculty, setFaculty] = useState('');
+  const [department, setDepartment] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+}
+
+return
+(
+    <div>
+
+    </div>
+)
+
+export default StudentRegister;
