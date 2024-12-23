ca8b4ea149254195c9a8156394321f434a437b00
Imalsha Fernando <139493584+ImalshaFernando@users.noreply.github.com>
2024-12-23 22:43:31 +0530

    Updating the Template.js

--- backend/models/Template.js
+++ backend/models/Template.js
@@ -5,53 +5,20 @@ const templateSchema = new mongoose.Schema({
     type: String,
     unique: true,
   },
-  templateName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["Students", "Staff"], 
-  },
-  hierarchy: [
-    {
-      position: {
-        type: String,
-        required: true,
-      },
-      staffId: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-  questions: [
-    {
-      question: {
+templateName: {
+type: String,
+required: true,
+@@ -31,7 +27,7 @@ const templateSchema = new mongoose.Schema({
+questions: [
+{
+question: {
         type: String
-      },
-      answerType: {
         type: String,
-        enum: ["text", "checkbox", "radio", "upload"],
-      },
-      options: [
-        {
-          type: String,
-        },
-      ],
-      visibility: {
-        type: String,
-        default: "all",
-      },
-    },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+},
+answerType: {
+type: String,
+@@ -54,11 +50,5 @@ const templateSchema = new mongoose.Schema({
+},
 });
 
 templateSchema.pre("save", function (next) {
