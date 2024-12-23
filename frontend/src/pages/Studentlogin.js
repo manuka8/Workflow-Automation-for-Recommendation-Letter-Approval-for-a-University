46db1555af81de2198727c12152b1fbedffb9318
madurangaPrabhath <maduracourse@gmail.com>
2024-12-23 19:26:17 +0530

    handle Login btn

--- frontend/src/pages/Studentlogin.js
+++ frontend/src/pages/Studentlogin.js
@@ -6,12 +6,37 @@ const Studentlogin = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/api/student/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ studentId, password }),
+      });
+
+      if (response.ok) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('ID', studentId);
+        localStorage.setItem('type', 'student');
+        console.log(data.token) // Store token if needed
+        navigate(`/studentdashboard`); // Redirect to student dashboard
+      } else {
+        setError(data.message || 'Login failed');
+      }
+    } catch (err) {
+      setError('An error occurred. Please try again.');
+      console.error(err);
+    }
+  };
   
   return (
     <View className="container">
       <div>
       <h2>Student Login</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         {error && <p style={{ color: 'red' }}>{error}</p>}
 
         <div>
