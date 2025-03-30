70a7a2d802708ee7368c0d2b6a3c90a0f198f481
RuzlaJabir <ruzlajabir@gmail.com>
2025-03-30 04:54:13 -0700

    Add route to fetch profile data based on userType

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -7,3 +7,23 @@ const path = require("path");
 const fs = require("fs");
 const Submitted = require('../models/Submitted');
 const bcrypt = require("bcrypt");
+
+router.get("/:id", async (req, res) => {
+    const { userType } = req.query;
+    const Model = userType === "student" ? Student : Staff;
+    
+    const userIdField = userType === "student" ? "studentId" : "staffId";
+  
+    try {
+      const user = await Model.findOne({ [userIdField]: req.params.id }).select("-password");
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+  
