7151e0526d67612d97e71b73c1da9e496fe1047b
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-04-02 07:20:22 +0530

    Create PendingApprovalsRoutes.js and update backend routes

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Connect to the database
+const studentRoutes = require('./routes/studentroutes');
+const templateRoutes = require('./routes/templateRoutes');
+const submissionsRoutes = require('./routes/submitRoutes');
+const staffRoutes = require('./routes/staffroutes');
+const pendingapprovalRoutes = require('./routes/PendingApprovalsRoutes');
 const connectDB = require('./config/db');
 connectDB();
 
@@ -18,6 +22,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json())
 
-//app.use('/login/student', studentRoute);
+app.use('/api/staff', staffRoutes);
+app.use('/api/submissions', submissionsRoutes);
+app.use('/api/student', studentRoutes);
+app.use('/api/pending-approvals', pendingapprovalRoutes);
+app.use('/api/templates', templateRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
